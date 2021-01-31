361760ae0a269b9839d3b5cedf6dc0e9b4bcc5bc
Eryk Kleczewski <MrNikoZero@gmail.com>
2021-01-31 12:25:47 +0100

    Rickandbrowser

---
+++ rickandbrowser/precache-manifest.fab442fba3a36a55fa5a26875631a83f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "698af084f028f98d2785f858402ea6d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c6d79f31b9f9459a342",
+    "url": "/static/css/main.c02b6063.chunk.css"
+  },
+  {
+    "revision": "1801426cc1e2a2ef6145",
+    "url": "/static/js/2.381f52f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.381f52f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c6d79f31b9f9459a342",
+    "url": "/static/js/main.b72fe3fb.chunk.js"
+  },
+  {
+    "revision": "98acb9a15adb0c031c37",
+    "url": "/static/js/runtime-main.244d7262.js"
+  }
+]);
